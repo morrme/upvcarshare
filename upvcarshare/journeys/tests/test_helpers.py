066865a1968ad01857fff9245eb399a99ad55acc
Marcos Gabarda <marcosgabarda@gmail.com>
2016-07-18 17:27:00 +0200

    more tests

--- upvcarshare/journeys/tests/test_helpers.py
+++ upvcarshare/journeys/tests/test_helpers.py
@@ -2,12 +2,15 @@
 from __future__ import unicode_literals, print_function, absolute_import
 
 import datetime
+
+import recurrence
 from django.contrib.gis.geos import Point
 from test_plus import TestCase
 
 from journeys import DEFAULT_PROJECTED_SRID, DEFAULT_WGS84_SRID
 from journeys.helpers import make_point_wgs84, make_point_projected, date_to_datetime, first_day_current_week, \
-    last_day_current_week
+    last_day_current_week, expand
+from journeys.tests.factories import JourneyFactory
 
 
 class JourneysHelpersTest(TestCase):
@@ -31,3 +34,18 @@ class JourneysHelpersTest(TestCase):
 
     def test_last_day_current_week(self):
         self.assertIsInstance(last_day_current_week(), datetime.date)
+
+    def test_expand(self):
+        journey = JourneyFactory()
+
+        rule = recurrence.Rule(recurrence.DAILY)
+        pattern = recurrence.Recurrence(
+            dtstart=datetime.datetime.now() + datetime.timedelta(days=1),
+            dtend=datetime.datetime.now() + datetime.timedelta(days=20),
+            rrules=[rule, ]
+        )
+
+        journey.recurrence = pattern
+        journey.save()
+        journeys = expand(journey)
+        self.assertEquals(45, len(journeys))
